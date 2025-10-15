c1d79f7ad53e5ec12a7280e3a4c09debde347469
youssef-moussa <youssef.m.moussa88@gmail.com>
2025-10-15 14:10:46 +0300

    AdminRole commit

---
+++ AdminRole.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class AdminRole {
+    private EmployeeUserDatabase database;
+    
+    public AdminRole(){
+        database = new EmployeeUserDatabase("Employees.txt");
+        database.readFromFile();
+    }
+
+    public void addEmployee(String employeeId,String name,String email,String address,String phoneNumber){
+        EmployeeUser employeeUser = new EmployeeUser(employeeId,name,email,address,phoneNumber);
+        database.insertRecord(employeeUser);
+    }
+
+    public EmployeeUser[] getListOfEmployees(){
+        ArrayList<EmployeeUser> records = database.returnAllRecords();
+        EmployeeUser[] employeeUsers;
+        employeeUsers = records.toArray(new EmployeeUser[0]);
+        return employeeUsers;
+    }
+
+    public void removeEmployee(String key){
+        database.deleteRecord(key);
+    }
+    public void logout(){
+        database.saveToFile();
+    }
+}
